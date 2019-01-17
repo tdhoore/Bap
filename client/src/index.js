f305a5b2541a6a49cbc7a4e63b4d0f6bc782b3a5
tdhoore <timdhoore@gmail.com>
2019-01-17 10:04:15 +0100

    base file done

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,34 @@
-require('./style.css');
+import "./style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+//import ApolloClient from 'apollo-boost';
+//import {ApolloProvider} from 'react-apollo';
+
+import store from "./store";
+
+/*const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  clientState: {
+    defaults,
+    resolvers
+  },
+  request: async operation => {
+    const token = localStorage.getItem("jwt");
+    operation.setContext({
+      headers: {
+        authorization: token
+          ? `Bearer ${token}`
+          : ""
+      }
+    });
+  }
+});*/
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App store={store} />
+  </BrowserRouter>,
+  document.getElementById(`root`)
+);
