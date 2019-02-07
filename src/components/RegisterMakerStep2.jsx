4d9f2b90894f3b01b8a34d1ab0056c5db8eb5600
tdhoore <timdhoore@gmail.com>
2019-02-07 18:45:10 +0100

    maker register done

--- src/components/RegisterMakerStep2.jsx
+++ src/components/RegisterMakerStep2.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import StepCounter from "./StepCounter.jsx";
 
 const RegisterMakerStep2 = ({ store }) => {
   const andereInputRef = React.createRef();
@@ -30,70 +31,84 @@ const RegisterMakerStep2 = ({ store }) => {
   };
 
   return (
-    <article className="">
+    <article className="loginHolder">
       <header>
         <h2>registreer</h2>
       </header>
-      <form className="">
-        <p className="">Wat zijn jouw hobby's?</p>
-        <label htmlFor="piano">
-          <span>Piano</span>
-          <input
-            type="checkbox"
-            name="hobby"
-            id="piano"
-            value="piano"
-            onChange={e => handleMakerHobby(e)}
-          />
-        </label>
-        <label htmlFor="tennis">
-          <span>Tennis</span>
-          <input
-            type="checkbox"
-            name="hobby"
-            id="tennis"
-            value="tennis"
-            onChange={e => handleMakerHobby(e)}
-          />
-        </label>
-        <label htmlFor="pottenbakken">
-          <span>Pottenbakken</span>
-          <input
-            type="checkbox"
-            name="hobby"
-            id="pottenbakken"
-            value="pottenbakken"
-            onChange={e => handleMakerHobby(e)}
-          />
-        </label>
-        <label htmlFor="gitaar">
-          <span>Gitaar</span>
-          <input
-            type="checkbox"
-            name="hobby"
-            id="gitaar"
-            value="gitaar"
-            onChange={e => handleMakerHobby(e)}
-          />
-        </label>
-        <label htmlFor="andereCheckbox">
-          <span>Andere:</span>
-          <input
-            type="checkbox"
-            name="hobby"
-            id="andereCheckbox"
-            onChange={e => handleMakerHobby(e)}
-          />
-          <input
-            type="text"
-            name="hobby"
-            onChange={e => handleMakerHobby(e)}
-            disabled
-            ref={andereInputRef}
-          />
-        </label>
-        <button onClick={e => handlePreviousPage(e)}>Vorige</button>
-        <button onClick={e => handleNextPage(e)}>Volgende</button>
+      <form className="colorBg colorBgTop lastSection">
+        <div className="formContentHolder">
+          <legend>Wat zijn jouw hobby's?</legend>
+          <label htmlFor="piano" className="labelWithCheckbox">
+            <span>Piano</span>
+            <input
+              type="checkbox"
+              name="hobby"
+              id="piano"
+              value="piano"
+              onChange={e => handleMakerHobby(e)}
+            />
+            <span className="customCheckBox" />
+          </label>
+          <label htmlFor="tennis" className="labelWithCheckbox">
+            <span>Tennis</span>
+            <input
+              type="checkbox"
+              name="hobby"
+              id="tennis"
+              value="tennis"
+              onChange={e => handleMakerHobby(e)}
+            />
+            <span className="customCheckBox" />
+          </label>
+          <label htmlFor="pottenbakken" className="labelWithCheckbox">
+            <span>Pottenbakken</span>
+            <input
+              type="checkbox"
+              name="hobby"
+              id="pottenbakken"
+              value="pottenbakken"
+              onChange={e => handleMakerHobby(e)}
+            />
+            <span className="customCheckBox" />
+          </label>
+          <label htmlFor="gitaar" className="labelWithCheckbox">
+            <span>Gitaar</span>
+            <input
+              type="checkbox"
+              name="hobby"
+              id="gitaar"
+              value="gitaar"
+              onChange={e => handleMakerHobby(e)}
+            />
+            <span className="customCheckBox" />
+          </label>
+          <label htmlFor="andereCheckbox" className="labelWithCheckbox">
+            <span>Andere:</span>
+            <input
+              type="checkbox"
+              name="hobby"
+              id="andereCheckbox"
+              onChange={e => handleMakerHobby(e)}
+            />
+            <input
+              type="text"
+              name="hobby"
+              onChange={e => handleMakerHobby(e)}
+              disabled
+              ref={andereInputRef}
+            />
+            <span className="customCheckBox" />
+          </label>
+        </div>
+        <StepCounter store={store} />
+        <div className="prevAndNextHolder">
+          <button onClick={e => handlePreviousPage(e)} className="ghostBtn">
+            Vorige
+          </button>
+          <button onClick={e => handleNextPage(e)} className="btn">
+            Volgende
+          </button>
+        </div>
       </form>
     </article>
   );
