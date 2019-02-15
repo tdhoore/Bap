bab6467a3ed1f268b05568e6688cccdc2d38b09c
tdhoore <timdhoore@gmail.com>
2019-02-15 09:41:28 +0100

    fixed peter

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,13 +1,14 @@
 import { observer } from "mobx-react";
 import React from "react";
 import { Redirect } from "react-router-dom";
+import peter from "assets/img/peter.svg";
 
 const Loading = ({ store, link }) => {
   if (store.loading) {
     return (
       <div className="loading-wrapper">
         <h3>{store.message}</h3>
-        <img src="assets/img/peter.svg" alt="peter" />
+        <img src={peter} alt="peter" />
         <p>
           Wist je dat.. <br /> D4E1 al sinds 2009 geweldige projecten
           produceert?
